2370712ced3bea067bdd56d67d4bb1b72043aea3
Angela Nikodinovska <nikodinovskaa4@hotmail.com>
2022-08-16 23:32:35 +0200

    WIP TransitionTable search works, but action call does not.

--- include/TransitionTable.hpp
+++ include/TransitionTable.hpp
@@ -1,6 +1,9 @@
 #ifndef CFSM_TRANSITION_TABLE_HPP
 #define CFSM_TRANSITION_TABLE_HPP
 
+#include <tuple>
+#include <optional>
+
 namespace Fsm
 {
   template <typename StateT, typename... Transitions>
@@ -8,15 +11,32 @@ namespace Fsm
   {
     public:
       template <typename EventT>
-      static StateT makeTransition(const StateT &currentState, const EventT& event)
+      static std::optional<StateT> makeTransition(const StateT currentState, const EventT &event)
       {
-        // find the transition
-        // do linear search? on transitions pack
-        // variadic templates, fold expressions...
-        // call action for the transition type
-        // return next state, from the transition type
+        static constexpr std::tuple<Transitions...> transitionsTuple{Transitions()...};
+        return getNextState(transitionsTuple, currentState, event);
       }
+
+    private:
+      template <std::size_t I = 0, typename EventT, typename... Ts>
+      static std::optional<StateT> getNextState(std::tuple<Ts...> tup, const StateT &currentState, const EventT &event)
+      {
+        if constexpr (I == sizeof...(Ts)) return {};
+        else
+        {
+          if (std::tuple_element<I, decltype(tup)>::type::stateFrom == currentState 
+              && std::tuple_element<I, decltype(tup)>::type::EventType::id() == EventT::id())
+          {
+            std::tuple_element<I, decltype(tup)>::type::action(event);
+            return std::tuple_element<I, decltype(tup)>::type::stateTo;
+          }
+          else
+            return getNextState<I + 1>(tup, currentState, event);
+        }
+      }
+    public:
+      static constexpr std::size_t tableSize_ = sizeof...(Transitions);
   };
 }
 
-#endif //CFSM_TRANSITION_TABLE_HPP
+#endif // CFSM_TRANSITION_TABLE_HPP
